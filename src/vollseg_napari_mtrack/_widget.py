b6450dd4008660995e525cf104d02db7ab2d624d
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-12-26 18:02:31 +0100

    green

--- src/vollseg_napari_mtrack/_widget.py
+++ src/vollseg_napari_mtrack/_widget.py
@@ -1164,10 +1164,12 @@ def plugin_wrapper_mtrack():
                         edge_color="red",
                         edge_width=1,
                     )
-        time.sleep(1)
-        plugin_ransac_parameters.recompute_current_button.native.setStyleSheet(
-            "background-color: green"
-        )
+
+        if worker is not None:
+            worker.quit()
+            plugin_ransac_parameters.recompute_current_button.native.setStyleSheet(
+                "background-color: green"
+            )
 
     # -> triggered by napari (if there are any open images on plugin launch)
 
