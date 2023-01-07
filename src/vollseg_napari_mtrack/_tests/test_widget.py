b8a980365ed9fd77e21fcafdbe3037dfa22f7e29
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-01-07 19:42:32 +0100

    version bump

--- src/vollseg_napari_mtrack/_tests/test_widget.py
+++ src/vollseg_napari_mtrack/_tests/test_widget.py
@@ -1,11 +1,11 @@
-import napari
+from vollseg_napari_mtrack import plugin_wrapper_mtrack
 
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_widget():
+def test_plugin_wrapper_mtrack(make_napari_viewer):
     # make viewer and add an image layer using our fixture
-    viewer = napari.Viewer()
+    viewer = make_napari_viewer()
     # create our widget, passing in the viewer
-    viewer.window.add_plugin_dock_widget("vollseg-napari-mtrack", "MTrack")
-    napari.run()
+    my_widget = plugin_wrapper_mtrack()
+    viewer.window.add_dock_widget(my_widget)
