78819ed6096bc24265a21ca42fbb194055d30e8a
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-01-27 16:49:55 +0100

    none

--- src/vollseg_napari_mtrack/_widget.py
+++ src/vollseg_napari_mtrack/_widget.py
@@ -357,7 +357,7 @@ def plugin_wrapper_mtrack():
         rate_calculator(ndim)
 
     def plot_main():
-        if len(plot_class.scroll_layout.count) > 0:
+        if plot_class.scroll_layout.count > 0:
             plot_class._reset_container(plot_class.scroll_layout)
         _refreshPlotData(table_tab._data.get_data())
 
